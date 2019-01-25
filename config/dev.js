dfdc2b7a0aa1a0d1bdbca9e5c82a219e86567cf4
mickey henry parker <henryparker594@gmail.com>
2019-01-25 18:58:08 -0500

    fix git ignore

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-mongoURI:'mongodb://henrysparebare:zBDt8EvVK7f8aGb@ds161794.mlab.com:61794/sparebare',
-PLAID_CLIENT_ID:"5c2c2f3948339d001160146f",
-PLAID_SECRET_KEY:"6c8c827b9701b137e5f91d6a60a165",
-PLAID_PUBLIC_KEY:"c34f505df53215f364ae16f9657339"
-}
-
